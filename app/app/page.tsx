2e612dd37ced7c1e84ebf03c7785c5d5324b7beb
Julio M Cruz <julio.cruz@eb-ms.net>
2024-09-03 15:27:59 -0400

    Update background

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -3,8 +3,8 @@ import ATMComponent from "../components/atm";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <div className="image-container ">
       <ATMComponent />
-    </main>
+    </div>
   );
 }
